67cf0cc881952cc7d06dd4597365986a7466df03
adrian <ingeflores916@gmail.com>
2020-03-29 20:16:09 -0700

    grupo

--- programa.c
+++ programa.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 main(){ 
 printf("TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA");
-printf("PEREZ BRIONES MARIA VICTORIA");       
+printf("PEREZ BRIONES MARIA VICTORIA");
+printf("1801");       
 }
 
